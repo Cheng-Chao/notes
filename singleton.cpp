225ebbedd7e1252f87717db12772a49f52d0fcc7
zhaojinzhou <1204416028@qq.com>
2017-07-25 19:34:12 +0800

    Update singleton.cpp

--- singleton.cpp
+++ singleton.cpp
@@ -1,23 +1,23 @@
 /*------------------use macros to define a Singleton----------------*/    	
-	#define DECLEAR_GET_INSTANCE(class_name)
-	public:                             \
-	static class_name* GetInstance()    \
-	{                                  \
-	  static class_name* instance = NULL;\
-	  if(NULL == instance)             \
-	  {                                \
-	    instance = new class_name;     \
-	  }                                \
-	  return instance;                 \
-	}                                   
+#define DECLEAR_GET_INSTANCE(class_name)
+public:                             \
+static class_name* GetInstance()    \
+{                                  \
+  static class_name* instance = NULL;\
+  if(NULL == instance)             \
+  {                                \
+    instance = new class_name;     \
+  }                                \
+  return instance;                 \
+}                                   
 
-	#define DECLARE_SINGLETON(class_name)  
-    DECLEAR_GET_INSTANCE(class_name)   \
-    protected:                         \
-    class_name();                      \
-    ~class_name();                     \
-    class_name(const class_name& );    \
-    const class_name& operator=(const class_name&);
+#define DECLARE_SINGLETON(class_name)  
+DECLEAR_GET_INSTANCE(class_name)   \
+protected:                         \
+class_name();                      \
+~class_name();                     \
+class_name(const class_name& );    \
+const class_name& operator=(const class_name&);
     
 //  using method;
 class singleton{
