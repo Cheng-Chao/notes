4731f303c8fbf89b0e978628f5eba7be502ec84e
zhaojinzhou <791620078@qq.com>
2018-01-11 10:29:38 +0800

    Create valid_initial_list.cpp

---
+++ cpp/valid_initial_list.cpp
@@ -0,0 +1,42 @@
+//#include "header.h"	//AnycodeX includes the header.h by default, needn't cancle the notation.
+#include <iostream>
+using namespace std;
+
+
+
+class pre{
+    public:
+    pre(){
+             cout<<"preconstr\t"<<endl;
+    }
+    pre(int i){
+        cout<<"preconstr\t"<<i<<endl;
+    }
+    
+    pre(const pre& input){
+        cout<<"copy const pre"<<endl;
+    }
+    const pre& operator=(const pre& input){
+        cout<<"operator = copy const pre"<<endl;
+    }
+};
+
+
+class later{
+    public:
+    later(pre& input){
+        inst = input;
+        cout<<"later const"<<endl;
+    }
+    
+    pre inst;
+};
+
+
+int main()
+{
+	pre exa(2);
+	
+	later exac(exa);
+	return 0;
+}
