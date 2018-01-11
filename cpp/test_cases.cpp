66d68283c149ce70c264bb8093bdb9c5be33c80e
zhaojinzhou <791620078@qq.com>
2018-01-11 11:08:25 +0800

    Update test_cases.cpp

--- cpp/test_cases.cpp
+++ cpp/test_cases.cpp
@@ -2,48 +2,6 @@
 #include <iostream>
 #include "test_initial_list.h"
 
-
-
-
-class pre{
-    public:
-    pre(){
-             cout<<"preconstr\t"<<endl;
-    }
-    pre(int i){
-        cout<<"preconstr\t"<<i<<endl;
-    }
-    
-    pre(const pre& input){
-        cout<<"copy const pre"<<endl;
-    }
-    const pre& operator=(const pre& input){
-        cout<<"operator = copy const pre"<<endl;
-    }
-};
-
-
-class later_normal{
-    public:
-    later_normal(pre& input){
-        inst = input;
-        cout<<"later const"<<endl;
-    }
-    
-    pre inst;
-};
-
-class later_initial_list{
-    public:
-    later_initial_list(pre& input):inst(input){
-          cout<<"later1 const"<<endl;
-    }
-    
-    pre inst;
-};
-
-
-
 void test_initial_list(){
 	pre exa(2);
 	cout<<"-----------end of construct of pre---------"<<endl;
