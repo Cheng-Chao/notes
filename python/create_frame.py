6b5bd527a41b5e348d6b5317817d74e040eecdda
zhaojinzhou <791620078@qq.com>
2018-03-18 00:54:37 +0800

    Create create_frame.py

---
+++ python/create_frame.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+from selenium import webdriver
+import Tkinter
+import time
+import tkMessageBox
+ 
+def creatframe():
+    root = Tkinter.Tk()
+    b = Tkinter.Button(root, text="zhaojinzhou shouji")
+    b.pack()
+    root.mainloop()
+ 
+
+time.sleep(1)
+creatframe()
+
